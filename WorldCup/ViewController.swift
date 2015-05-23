ef457ca2577ce7197287bc2db1dbe6ae95e989f2
Aaron Bradley <abeats20@gmail.com>
2015-05-23 17:38:56 -0500

    NSFetchedResultsController playing nicely with CoreData and TableViews

--- WorldCup/ViewController.swift
+++ WorldCup/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NSFetchedResultsControllerDelegate {
 
   var coreDataStack: CoreDataStack!
   var fetchedResultsController: NSFetchedResultsController!
@@ -27,13 +27,92 @@ class ViewController: UIViewController {
     let nameSort = NSSortDescriptor(key: "teamName", ascending: true)
 
     fetchRequest.sortDescriptors = [zoneSort, scoreSort, nameSort]
-    fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: coreDataStack.context, sectionNameKeyPath: "qualifyingZone", cacheName: nil)
+    fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: coreDataStack.context, sectionNameKeyPath: "qualifyingZone", cacheName: "worldCup")
+    fetchedResultsController.delegate = self
 
     var error: NSError? = nil
     if (!fetchedResultsController.performFetch(&error)) {
       println("Error: \(error?.localizedDescription)")
     }
 
+  }
+
+  func controllerWillChangeContent(controller: NSFetchedResultsController) {
+    tableView.beginUpdates()
+  }
+
+  func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
+    switch type {
+    case .Insert:
+      tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Automatic)
+    case .Delete:
+      tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Automatic)
+    case .Update:
+      let cell = tableView.cellForRowAtIndexPath(indexPath!) as! TeamCell
+      configureCell(cell, indexPath: indexPath!)
+    case .Move:
+      tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Automatic)
+      tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Automatic)
+    default:
+      break
+    }
+  }
+
+  func controllerDidChangeContent(controller: NSFetchedResultsController) {
+    tableView.endUpdates()
+  }
+
+  func controller(controller: NSFetchedResultsController, didChangeSection sectionInfo: NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
+
+    let indexSet = NSIndexSet(index: sectionIndex)
+
+    switch type {
+    case .Insert:
+      tableView.insertSections(indexSet, withRowAnimation: .Automatic)
+    case .Delete:
+      tableView.deleteSections(indexSet, withRowAnimation: .Automatic)
+    default:
+      break
+
+    }
+  }
+
+  override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent) {
+    if motion == .MotionShake {
+      addButton.enabled = true
+    }
+  }
+
+  @IBAction func addTeam(sender: AnyObject) {
+
+    var alert = UIAlertController(title: "Secret Team", message: "Add a new team", preferredStyle: UIAlertControllerStyle.Alert)
+
+    alert.addTextFieldWithConfigurationHandler { (textField: UITextField!) -> Void in
+      textField.placeholder = "Team Name"
+    }
+    alert.addTextFieldWithConfigurationHandler { (textField: UITextField!) -> Void in
+      textField.placeholder = "Qualifying Zone"
+    }
+
+    alert.addAction(UIAlertAction(title: "Save", style: .Default, handler: { (action: UIAlertAction!) -> Void in
+      println("Saved")
+
+      let nameTextField = alert.textFields![0] as! UITextField
+      let zoneTextField = alert.textFields![1] as! UITextField
+      let team = NSEntityDescription.insertNewObjectForEntityForName("Team", inManagedObjectContext: self.coreDataStack.context) as! Team
+
+      team.teamName = nameTextField.text
+      team.qualifyingZone = zoneTextField.text
+      team.imageName = "wenderland-flag"
+
+      self.coreDataStack.saveContext()
+    }))
+
+    alert.addAction(UIAlertAction(title: "Cancel", style: .Default, handler: { (action: UIAlertAction!) -> Void in
+      println("Cancel")
+    }))
+
+    presentViewController(alert, animated: true, completion: nil)
   }
 
   func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -68,13 +147,12 @@ class ViewController: UIViewController {
     let wins = team.wins.integerValue
     team.wins = NSNumber(integer: wins + 1)
     coreDataStack.saveContext()
-    tableView.reloadData()
   }
 
   func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
     let sectionInfo = fetchedResultsController.sections![section] as! NSFetchedResultsSectionInfo
     return sectionInfo.name
   }
-
+  
 }
 
